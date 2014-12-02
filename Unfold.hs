bf7079592192b58e76d1d85bc13ab1c76c0d4eec
Chris <cristoir@gmail.com>
2014-12-02 22:51:21 +0000

    starting to get hard...

---
+++ Unfold.hs
@@ -0,0 +1,15 @@
+type Bit = Int
+
+int2bin :: Int -> [Bit]
+int2bin 0 = []
+int2bin n = n`mod`2 : int2bin(n`div`2)
+
+unfold p h t x
+  | p x = []
+  | otherwise = h x : unfold p h t (t x)
+  
+int2bin' = unfold(==0)(`mod`2)(`div`2)
+
+chop8 :: [Bit] -> [[Bit]]
+chop8 [] = []
+chop8 bits = take 8 bits : chop8 (drop 8 bits)
